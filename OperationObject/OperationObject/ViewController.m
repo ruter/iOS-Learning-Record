62c1f71285be906a2532465917a2a07b96c43d5d
Ruter <ruter.lu@gmail.com>
2016-02-13 18:29:56 +0800

    Operation Object

--- OperationObject/OperationObject/ViewController.m
+++ OperationObject/OperationObject/ViewController.m
@@ -19,6 +19,33 @@
   // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)start:(id)sender {
+  NSInvocationOperation *op = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(loop) object:nil];
+  // 创建并添加操作队列
+  NSOperationQueue *q = [[NSOperationQueue alloc] init];
+  [q addOperation:op];
+  
+  NSLog(@"NSInvocationOperation END");
+}
+
+- (IBAction)block:(id)sender {
+  NSBlockOperation *bop = [NSBlockOperation blockOperationWithBlock:^{
+    [self loop];
+  }];
+  
+  NSOperationQueue *q = [[NSOperationQueue alloc] init];
+  [q addOperation:bop];
+
+  NSLog(@"NSBlockOperation END");
+}
+
+-(void)loop {
+  for (int i = 1; i <= 10; i++) {
+    [NSThread sleepForTimeInterval:1];
+    NSLog(@"i = %d", i);
+  }
+}
+
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
   // Dispose of any resources that can be recreated.
